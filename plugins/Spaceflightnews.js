a144474ad9a295405604abd2c17bea88e711f12e
Vai838 <75434115+Vai838@users.noreply.github.com>
2021-03-28 21:30:39 +0530

    Update Spaceflightnews.js

--- plugins/Spaceflightnews.js
+++ plugins/Spaceflightnews.js
@@ -18,36 +18,36 @@ Asena.addCommand({pattern: 'sfnews ?(.*)', fromMe: false, desc: Lang.NEWS_DESC},
 		const response = await got(url);
 		const json = JSON.parse(response.body);
 	        return await message.client.sendMessage(message.jid,'*📁 ' + "Spaceflight News" +'* \n\n\n' +
-		'*🗞️ ' + "Title" +':* ```' + json.data[0].title + '```\n' + 
-                '*📰 ' + "Summary" +':* ```' + json.data[0].summary + '```\n' + 
-		'*🔗 ' + Lang.RMLINK +':* ```' + json.data[0].url + '```\n\n' +
-                '*🗞️ ' + "Title" +':* ```' + json.data[1].title + '```\n' +                                                                         
-		'*📰 ' + "Summary" +':* ```' + json.data[1].summary + '```\n' + 
-		'*🔗 ' + Lang.RMLINK +':* ```' + json.data[1].url + '```\n\n' + 
-                '*🗞️ ' + "Title"+':* ```' + json.data[2].title + '```\n' +
-                '*📰 ' + "Summary" +':* ```' + json.data[2].summary + '```\n' + 
-		'*🔗 ' + Lang.RMLINK +':* ```' + json.data[2].url + '```\n\n' + 
-                '*🗞️ ' + "Title" +':* ```' + json.data[3].title + '```\n' +
-   	        '*📰 ' + "Summary" +':* ```' + json.data[3].summary + '```\n' + 
-		'*🔗 ' + Lang.RMLINK +':* ```' + json.data[3].url + '```\n\n' + 
-                '*🗞️ ' + "Title" +':* ```' + json.data[4].title + '```\n' +                                                                         
-                '*📰 ' + "Summary" +':* ```' + json.data[4].summary + '```\n' + 
-		'*🔗 ' + Lang.RMLINK +':* ```' + json.data[4].url + '```\n\n'+ 
-		 '*🗞️ ' + "Title" +':* ```' + json.data[5].title + '```\n' +                                                                         
-                '*📰 ' + "Summary" +':* ```' + json.data[5].summary + '```\n' + 
-		'*🔗 ' + Lang.RMLINK +':* ```' + json.data[5].url + '```\n\n'+
-		 '*🗞️ ' + "Title" +':* ```' + json.data[6].title + '```\n' +                                                                         
-                '*📰 ' + "Summary" +':* ```' + json.data[6].summary + '```\n' + 
-		'*🔗 ' + Lang.RMLINK +':* ```' + json.data[6].url + '```\n\n'+									 
-		 '*🗞️ ' + "Title" +':* ```' + json.data[7].title + '```\n' +                                                                         
-                '*📰 ' + "Summary" +':* ```' + json.data[7].summary + '```\n' + 
-		'*🔗 ' + Lang.RMLINK +':* ```' + json.data[7].url + '```\n\n'+									 
-		 '*🗞️ ' + "Title" +':* ```' + json.data[8].title + '```\n' +                                                                         
-                '*📰 ' + "Summary" +':* ```' + json.data[8].summary + '```\n' + 
-		'*🔗 ' + Lang.RMLINK +':* ```' + json.data[8].url + '```\n\n'+
-		'*🗞️ ' + "Title" +':* ```' + json.data[9].title + '```\n' +                                                                         
-                '*📰 ' + "Summary" +':* ```' + json.data[9].summary + '```\n' + 
-		'*🔗 ' + Lang.RMLINK +':* ```' + json.data[9].url + '```\n\n', MessageType.text);
+		'*🗞️ ' + "Title" +':* ```' + json[0].title + '```\n' + 
+                '*📰 ' + "Summary" +':* ```' + json[0].summary + '```\n' + 
+		'*🔗 ' + Lang.RMLINK +':* ```' + json[0].url + '```\n\n' +
+                '*🗞️ ' + "Title" +':* ```' + json[1].title + '```\n' +                                                                         
+		'*📰 ' + "Summary" +':* ```' + json[1].summary + '```\n' + 
+		'*🔗 ' + Lang.RMLINK +':* ```' + json[1].url + '```\n\n' + 
+                '*🗞️ ' + "Title"+':* ```' + json[2].title + '```\n' +
+                '*📰 ' + "Summary" +':* ```' + json[2].summary + '```\n' + 
+		'*🔗 ' + Lang.RMLINK +':* ```' + json[2].url + '```\n\n' + 
+                '*🗞️ ' + "Title" +':* ```' + json[3].title + '```\n' +
+   	        '*📰 ' + "Summary" +':* ```' + json[3].summary + '```\n' + 
+		'*🔗 ' + Lang.RMLINK +':* ```' + json[3].url + '```\n\n' + 
+                '*🗞️ ' + "Title" +':* ```' + json[4].title + '```\n' +                                                                         
+                '*📰 ' + "Summary" +':* ```' + json[4].summary + '```\n' + 
+		'*🔗 ' + Lang.RMLINK +':* ```' + json[4].url + '```\n\n'+ 
+		 '*🗞️ ' + "Title" +':* ```' + json[5].title + '```\n' +                                                                         
+                '*📰 ' + "Summary" +':* ```' + json[5].summary + '```\n' + 
+		'*🔗 ' + Lang.RMLINK +':* ```' + json[5].url + '```\n\n'+
+		 '*🗞️ ' + "Title" +':* ```' + json[6].title + '```\n' +                                                                         
+                '*📰 ' + "Summary" +':* ```' + json[6].summary + '```\n' + 
+		'*🔗 ' + Lang.RMLINK +':* ```' + json[6].url + '```\n\n'+									 
+		 '*🗞️ ' + "Title" +':* ```' + json[7].title + '```\n' +                                                                         
+                '*📰 ' + "Summary" +':* ```' + json[7].summary + '```\n' + 
+		'*🔗 ' + Lang.RMLINK +':* ```' + json[7].url + '```\n\n'+									 
+		 '*🗞️ ' + "Title" +':* ```' + json[8].title + '```\n' +                                                                         
+                '*📰 ' + "Summary" +':* ```' + json[8].summary + '```\n' + 
+		'*🔗 ' + Lang.RMLINK +':* ```' + json[8].url + '```\n\n'+
+		'*🗞️ ' + "Title" +':* ```' + json[9].title + '```\n' +                                                                         
+                '*📰 ' + "Summary" +':* ```' + json[9].summary + '```\n' + 
+		'*🔗 ' + Lang.RMLINK +':* ```' + json[9].url + '```\n\n', MessageType.text);
 	} catch {
 		return await message.client.sendMessage(message.jid, "*Error*", MessageType.text);
 	}
