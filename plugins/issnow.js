b6d6f138d13db67e455c54a0a0965de291c7c95f
Vai838 <75434115+Vai838@users.noreply.github.com>
2021-03-29 00:05:07 +0530

    Update issnow.js

--- plugins/issnow.js
+++ plugins/issnow.js
@@ -25,3 +25,45 @@ Asena.addCommand({pattern: 'issnow ?(.*)', fromMe: false, desc: "Provides the cu
 		return await message.client.sendMessage(message.jid, "*Error*", MessageType.text);
 	}
 });
+
+Asena.addCommand({pattern: 'isspass ?(.*)', fromMe: false, desc: "Provides details of the passing of iss over a particular location.\n Provide lattitude and longitude for location.\n *Example: /isspass 12.3;75.1* \n You can use /weather <place_name> for getting lattitude and longitude of a place." }, async (message, match) => {
+	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
+	var topText, bottomText;
+	
+    if (match[1].includes(';')) {
+        var split = match[1].split(';');
+        longitude = split[1];
+        lattitude = split[0];
+}
+
+	
+	const url = `http://api.open-notify.org/iss-pass.json?lat=${lattitude}&lon=${longitude}`;
+	try {
+		const response = await got(url);
+		const json = JSON.parse(response.body);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🕰  ' + "UNIX Epoch time:" +'* ```' + json.request.datetime + '```\n' +
+                '*🌐  ' + "Lattitude:" +'* ```' + json.request.latitude + '```\n' +                                                                        
+		'*🌐 ' + "Longitude:" +'* ```' + json.request.longitude + '```\n' +
+		'*🛰 ' + "No of Passes:" +'* ```' + json.request.passes + '```\n\n' +
+											 
+		'*⏰ ' + "Pass_1 Duration(sec):" +'* ```' + json.response[0].duration + '```\n' +
+                '*🕰  ' + "Pass_1 Risetime(epoch):" +'* ```' + json.response[0].risetime + '```\n\n' + 
+											 
+	       '*⏰ ' + "Pass_1 Duration(sec):" +'* ```' + json.response[1].duration + '```\n' +
+                '*🕰  ' + "Pass_1 Risetime(epoch):" +'* ```' + json.response[1].risetime + '```\n\n' + 										 
+		
+	       '*⏰ ' + "Pass_1 Duration(sec):" +'* ```' + json.response[2].duration + '```\n' +
+                '*🕰  ' + "Pass_1 Risetime(epoch):" +'* ```' + json.response[2].risetime + '```\n\n' + 	
+											 
+	        '*⏰ ' + "Pass_1 Duration(sec):" +'* ```' + json.response[3].duration + '```\n' +
+                '*🕰  ' + "Pass_1 Risetime(epoch):" +'* ```' + json.response[3].risetime + '```\n\n' + 	
+											 
+	        '*⏰ ' + "Pass_1 Duration(sec):" +'* ```' + json.response[4].duration + '```\n' +
+                '*🕰  ' + "Pass_1 Risetime(epoch):" +'* ```' + json.response[4].risetime + '```\n\n' + 	
+											 
+	        '*⏰ ' + "Pass_1 Duration(sec):" +'* ```' + json.response[5].duration + '```\n' +
+                '*🕰  ' + "Pass_1 Risetime(epoch):" +'* ```' + json.response[5].risetime + '```\n\n' , MessageType.text);
+	} catch {
+		return await message.client.sendMessage(message.jid, "*Error*", MessageType.text);
+	}
+});
