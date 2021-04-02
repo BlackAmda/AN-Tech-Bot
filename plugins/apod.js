e89e9338e50b233a986885062b4f0fa176a4e496
Vai838 <75434115+Vai838@users.noreply.github.com>
2021-04-02 16:56:34 +0530

    Create apod.js

---
+++ plugins/apod.js
@@ -0,0 +1,46 @@
+const Asena = require('../events');
+const { MessageType } = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const { errorMessage, infoMessage } = require('../helpers');
+const Lang = Language.getString('instagram') ;
+
+
+
+
+
+Asena.addCommand({ pattern: 'apod ?(.*)', fromMe: false, desc: "Provides the Astronomy Picture Of the Day from the NASA site." }, async (message, match) => {
+
+
+    await message.sendMessage(infoMessage(Lang.LOADING))
+
+    await axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=ccdRSkRerDoJHn2g36DGnJgyctfkGDml3ZzMWWxL`)
+      .then(async (response) => {
+        const {
+          copyright,
+          date,
+          explanation,
+          hdurl,
+          title,
+        } = response.data
+
+        const profileBuffer = await axios.get(hdurl, {responseType: 'arraybuffer'})
+
+        const msg = `
+        *${"Title"}*: ${title}
+        *${"Copyright"}*: ${copyright}    
+        *${"Date"}*: ${date}
+        *${Explanation}*: ${explanation}`
+
+        await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
+          caption: msg, quoted: message.data
+        })
+      })
+      .catch(
+        async (err) => await message.sendMessage(errorMessage("Error")),
+      )
+  },
+)
+
